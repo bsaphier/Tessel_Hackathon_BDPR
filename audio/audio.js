75542c12e03d06539a9572af8d8572cdef66cd87
Benjamin Saphier <b.saphier@gmail.com>
2016-11-21 16:52:28 -0500

    default commit message

--- audio/audio.js
+++ audio/audio.js
@@ -1,10 +1,53 @@
-var path = require('path');
-var av = require('tessel-av');
-var mp3 = path.join(__dirname, '178617__goup-1__amen-break-j.mp3');
-var sound = new av.Speaker(mp3);
+const path = require('path');
+const av = require('tessel-av');
+const http = require('http');
 
-sound.play();
+// const mp3 = path.join(__dirname, '178617__goup-1__amen-break-j.mp3');
 
-sound.on('ended', function(seconds) {
-  sound.play();
+var audio = require('audio-vs1053b').use(tessel.port['A']);
+
+var chunks = [];
+
+// When we get data, push it into our array
+audio.on('data', function(data) {
+  chunks.push(data);
+});
+
+// Wait for the module to connect
+audio.on('ready', function() {
+  console.log('Hold the config button to record...');
+  // When the config button is pressed, start recording
+  tessel.button.once('press', startRecording);
+});
+
+
+function startRecording() {
+  // Tell the audio module to start recording
+  audio.startRecording('voice', function() {
+    console.log('Recording...');
+    // Once the button is released, stop recording
+    tessel.button.once('release', stopRecording);
+  });
+}
+
+function stopRecording() {
+  // Tell the audio module to stop recording
+  console.log('stopping the recording...');
+  audio.stopRecording(function() {
+    console.log('Playing it back...');
+    // Concat the data and play it
+    audio.play(Buffer.concat(chunks), function(err) {
+      // When we're done playing, clear recordings
+      chunks = [];
+      console.log('Hold the config button to record...');
+      // Wait for a button press again
+      tessel.button.once('press', startRecording);
+    });
+  });
+}
+
+
+// If there is an error, report it
+audio.on('error', function(err) {
+  throw err;
 });
